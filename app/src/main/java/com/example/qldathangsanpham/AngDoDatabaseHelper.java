33f7e9dec49af892ff441426c3072da88d9ce0fb
tuong1008 <tuong100852@gmail.com>
2022-04-13 20:30:13 +0700

    first commit

---
+++ app/src/main/java/com/example/qldathangsanpham/AngDoDatabaseHelper.java
@@ -0,0 +1,51 @@
+package com.example.qldathangsanpham;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AngDoDatabaseHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "QLDatHangSanPham"; // the name of our database
+    private static final int DB_VERSION = 1; // the version of the database
+
+    public AngDoDatabaseHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        updateMyDatabase(db, 0, DB_VERSION);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateMyDatabase(db, oldVersion, newVersion);
+    }
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion < 1) {
+            db.execSQL("CREATE TABLE KHACHHANG (maKH INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "tenKH TEXT, "
+                    + "diaChi TEXT, "
+                    + "soDT TEXT, "
+                    + "avatar INTEGER);");
+            insertDrink(db, "Latte", "Espresso and steamed milk", "0334139916", R.drawable.duck);
+        }
+//        if (oldVersion <2){
+//            db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC;");
+//        }
+    }
+
+    private static void insertDrink(SQLiteDatabase db, String tenKH, String diaChi,
+                                    String soDT, int resourceId) {
+        ContentValues customerValues = new ContentValues();
+        customerValues.put("tenKH", tenKH);
+        customerValues.put("diaChi", diaChi);
+        customerValues.put("soDT", soDT);
+        customerValues.put("avatar", resourceId);
+        db.insert("KHACHHANG", null, customerValues);
+    }
+}
---
+++ app/src/main/java/com/example/qldathangsanpham/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.qldathangsanpham;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView l;
+    String menu[]
+            = { "Quản lý Đơn đặt hàng", "Quản lý Thông tin khách hàng",
+            "Quản lý Sản phẩm"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+
+//        SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), getLifecycle());
+//        ViewPager2 pager = findViewById(R.id.pager);
+//        pager.setAdapter(pagerAdapter);
+//
+//        TabLayout tabLayout = findViewById(R.id.tabs);
+//        new TabLayoutMediator(tabLayout, pager,
+//                (tab, position) -> tab.setText("OBJECT " + (position + 1))
+//        ).attach();
+
+        l = findViewById(R.id.list);
+        ArrayAdapter<String> arr;
+        arr = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                menu);
+        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                switch (position){
+                    case 1:
+                        Intent intent = new Intent(MainActivity.this, CustomerActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+        l.setAdapter(arr);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_create_order:
+                Intent intent = new Intent(this, ProfileActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
