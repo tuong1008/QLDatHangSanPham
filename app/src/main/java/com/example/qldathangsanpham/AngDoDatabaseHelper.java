3cade91bb25237ebeb21fcf151db415ae2e0b1ab
tuong1008 <tuong100852@gmail.com>
2022-04-17 08:23:02 +0700

    complete crud customer

--- app/src/main/java/com/example/qldathangsanpham/AngDoDatabaseHelper.java
+++ app/src/main/java/com/example/qldathangsanpham/AngDoDatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.qldathangsanpham;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -31,21 +32,48 @@ public class AngDoDatabaseHelper extends SQLiteOpenHelper {
                     + "tenKH TEXT, "
                     + "diaChi TEXT, "
                     + "soDT TEXT, "
-                    + "avatar INTEGER);");
-            insertDrink(db, "Latte", "Espresso and steamed milk", "0334139916", R.drawable.duck);
+                    + "avatar TEXT);");
         }
 //        if (oldVersion <2){
 //            db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC;");
 //        }
     }
 
-    private static void insertDrink(SQLiteDatabase db, String tenKH, String diaChi,
-                                    String soDT, int resourceId) {
+    public static int nextAutoIncrement(SQLiteDatabase db, String table, String primaryKey){
+        String query = "SELECT MAX("+primaryKey+") AS max_id FROM "+ table;
+        Cursor cursor = db.rawQuery(query, null);
+
+        int id = 0;
+        if (cursor.moveToFirst())
+        {
+            do
+            {
+                id = cursor.getInt(0);
+            } while(cursor.moveToNext());
+        }
+        return id+1;
+    }
+
+    public static void insertCustomer(SQLiteDatabase db, String tenKH, String diaChi,
+                                    String soDT, String avatarPath) {
         ContentValues customerValues = new ContentValues();
         customerValues.put("tenKH", tenKH);
         customerValues.put("diaChi", diaChi);
         customerValues.put("soDT", soDT);
-        customerValues.put("avatar", resourceId);
+        customerValues.put("avatar", avatarPath);
         db.insert("KHACHHANG", null, customerValues);
+    }
+
+    public static void updateCustomer(SQLiteDatabase db, String tenKH, String diaChi,
+                                      String soDT, String maKH) {
+        ContentValues customerValues = new ContentValues();
+        customerValues.put("tenKH", tenKH);
+        customerValues.put("diaChi", diaChi);
+        customerValues.put("soDT", soDT);
+        db.update("KHACHHANG", customerValues, "maKH = ?", new String[] {maKH});
+    }
+
+    public static void deleteCustomer(SQLiteDatabase db, String maKH) {
+        db.delete("KHACHHANG", "maKH = ?", new String[] {maKH});
     }
 }
--- app/src/main/java/com/example/qldathangsanpham/CustomerActivity.java
+++ app/src/main/java/com/example/qldathangsanpham/CustomerActivity.java
@@ -3,16 +3,32 @@ package com.example.qldathangsanpham;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cursoradapter.widget.CursorAdapter;
+import androidx.cursoradapter.widget.SimpleCursorAdapter;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class CustomerActivity extends AppCompatActivity {
 
+    private SQLiteDatabase db;
+    private Cursor customersCursor;
+    public static  final String EXTRA_MAKH = "maKH";
+
     public void onClickAdd(View view){
         Intent intent = new Intent(this, CustomerFormActivity.class);
+        intent.putExtra(EXTRA_MAKH, -1);
         startActivity(intent);
     }
 
@@ -24,5 +40,64 @@ public class CustomerActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+        setCustomersListView();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Cursor newCursor = db.query("KHACHHANG", new String[]{"maKH as " + BaseColumns._ID, "tenKH"},
+                null, null, null, null, null);
+
+        ListView listFavorites = findViewById(R.id.list);
+        CursorAdapter adapter = (CursorAdapter) listFavorites.getAdapter();
+        adapter.changeCursor(newCursor);
+        customersCursor = newCursor;
+    }
+
+
+    private void setCustomersListView(){
+        ListView listCustomers = findViewById(R.id.list);
+        try{
+            SQLiteOpenHelper angDoDatabaseHelper = new AngDoDatabaseHelper(this);
+            db = angDoDatabaseHelper.getReadableDatabase();
+            customersCursor = db.query("KHACHHANG", new String[]{"maKH as " + BaseColumns._ID, "tenKH"},
+                    null, null, null, null, null);
+            CursorAdapter customerAdapter =
+                    new SimpleCursorAdapter(CustomerActivity.this, android.R.layout.simple_list_item_1, customersCursor, new String[]{"tenKH"}, new int[]{android.R.id.text1},0);
+            listCustomers.setAdapter(customerAdapter);
+
+//            String tableName = "KhachHang";
+//            Log.d("LogCursor", "getTableAsString called");
+//            String tableString = String.format("Table %s:\n", tableName);
+//            Cursor allRows  = customersCursor;
+//            if (allRows.moveToFirst() ){
+//                String[] columnNames = allRows.getColumnNames();
+//                do {
+//                    for (String name: columnNames) {
+//                        tableString += String.format("%s: %s\n", name,
+//                                allRows.getString(allRows.getColumnIndex(name)));
+//                    }
+//                    tableString += "\n";
+//
+//                } while (allRows.moveToNext());
+//                Log.d("LogCursor", tableString);
+//            }
+        }
+        catch (SQLiteException e){
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+            e.printStackTrace();
+        }
+        listCustomers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(CustomerActivity.this, CustomerFormActivity.class);
+                intent.putExtra(EXTRA_MAKH, (int) id);
+                startActivity(intent);
+            }
+        });
+    }
+
+
 }
