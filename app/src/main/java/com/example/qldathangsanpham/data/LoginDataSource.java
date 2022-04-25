64b932bfe9fc0b68a5301913bd0aaab3cc2660e3
zoroONE01 <lekimnguyen671@gmail.com>
2022-04-25 23:47:26 +0700

    encrypt password

--- app/src/main/java/com/example/qldathangsanpham/data/LoginDataSource.java
+++ app/src/main/java/com/example/qldathangsanpham/data/LoginDataSource.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.qldathangsanpham.DatabaseHelper;
+import com.example.qldathangsanpham.Utility;
 import com.example.qldathangsanpham.data.model.LoggedInUser;
 
 import java.io.IOException;
@@ -20,51 +21,46 @@ public class LoginDataSource {
 
     public LoginDataSource(Context mcontext) {
         this.mcontext = mcontext;
-        mDatabase=new DatabaseHelper(mcontext).getWritableDatabase();
+        mDatabase = new DatabaseHelper(mcontext).getWritableDatabase();
     }
+
     public Result<LoggedInUser> login(String username, String password) {
         try {
             // TODO: handle loggedInUser authentication
-            mDatabase=new DatabaseHelper(mcontext).getWritableDatabase();
-            Cursor  cursor=mDatabase.query(DatabaseHelper.TB_TAI_KHOAN_NHAN_VIEN,null,"username=?",new String[]{username},null,null,null);
-            if(cursor.moveToFirst())
-            {
-                String id=cursor.getString(0);
-                String userName=cursor.getString(1);
-                String passWord=cursor.getString(2);
-                if(userName.equals(username)&&passWord.equals(password))
-                {
-                    LoggedInUser loggedInUser=new LoggedInUser(id,userName);
+            mDatabase = new DatabaseHelper(mcontext).getWritableDatabase();
+            Cursor cursor = mDatabase.query(DatabaseHelper.TB_TAI_KHOAN_NHAN_VIEN, null, "username=?", new String[]{username}, null, null, null);
+            if (cursor.moveToFirst()) {
+                String id = cursor.getString(0);
+                String userName = cursor.getString(1);
+                String passWord = cursor.getString(2);
+                if (userName.equals(username) && Utility.checkPassword(password, passWord)) {
+                    LoggedInUser loggedInUser = new LoggedInUser(id, userName);
                     return new Result.Success<>(loggedInUser);
-                }
-                else{
+                } else {
                     return new Result.Error(new RuntimeException("Username or password invalid"));
                 }
-            }
-            else{
+            } else {
                 return new Result.Error(new RuntimeException("Username or password not found"));
             }
 
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
-        }
-        finally {
+        } finally {
             mDatabase.close();
         }
     }
 
-    public Boolean changePassword(String username,String password,String newPassword){
-        try{
-            mDatabase=new DatabaseHelper(mcontext).getWritableDatabase();
-            ContentValues values=new ContentValues();
-            values.put(DatabaseHelper.CL_PASSWORD,newPassword);
-         if(mDatabase.update(DatabaseHelper.TB_TAI_KHOAN_NHAN_VIEN,values,DatabaseHelper.CL_USERNAME+"=? and "+DatabaseHelper.CL_PASSWORD+"=?",new String[]{username,password})>0){
-             return true;
-         }
-         else{
-             return false;
-         }
-        }finally {
+    public Boolean changePassword(String username, String password, String newPassword) {
+        try {
+            mDatabase = new DatabaseHelper(mcontext).getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put(DatabaseHelper.CL_PASSWORD, newPassword);
+            if (mDatabase.update(DatabaseHelper.TB_TAI_KHOAN_NHAN_VIEN, values, DatabaseHelper.CL_USERNAME + "=? and " + DatabaseHelper.CL_PASSWORD + "=?", new String[]{username, password}) > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } finally {
             mDatabase.close();
         }
 
--- app/src/main/java/com/example/qldathangsanpham/data/RegisterDataSource.java
+++ app/src/main/java/com/example/qldathangsanpham/data/RegisterDataSource.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.qldathangsanpham.DatabaseHelper;
+import com.example.qldathangsanpham.Utility;
 import com.example.qldathangsanpham.data.model.RegisterUser;
 
 import java.io.IOException;
@@ -18,33 +19,31 @@ public class RegisterDataSource {
     private SQLiteDatabase mDatabase;
 
     public RegisterDataSource(Context mcontext) {
-        this.mcontext=mcontext;
-        mDatabase=new DatabaseHelper(mcontext).getWritableDatabase();
+        this.mcontext = mcontext;
+        mDatabase = new DatabaseHelper(mcontext).getWritableDatabase();
     }
 
-    public Result<RegisterUser> register(String username, String password,byte [] avatar,String hoVaTen) {
-        mDatabase=new DatabaseHelper(mcontext).getWritableDatabase();
+    public Result<RegisterUser> register(String username, String password, byte[] avatar, String hoVaTen) {
+        mDatabase = new DatabaseHelper(mcontext).getWritableDatabase();
         try {
             // TODO: handle loggedInUser authentication
-            ContentValues taiKhoanValue=new ContentValues();
-            taiKhoanValue.put("username",username);
-            taiKhoanValue.put("password",password);
-            mDatabase.insert(DatabaseHelper.TB_TAI_KHOAN_NHAN_VIEN,null,taiKhoanValue);
+            ContentValues taiKhoanValue = new ContentValues();
+            taiKhoanValue.put("username", username);
+            taiKhoanValue.put("password", Utility.encryptPassword(password));
+            mDatabase.insert(DatabaseHelper.TB_TAI_KHOAN_NHAN_VIEN, null, taiKhoanValue);
             int id;
-            Cursor cursor=mDatabase.query(DatabaseHelper.TB_TAI_KHOAN_NHAN_VIEN,null,DatabaseHelper.CL_USERNAME+"=?",new String[]{username},null,null,null);
-            if(cursor.moveToFirst())
-            {
-                ContentValues nhanVienValue=new ContentValues();
-                nhanVienValue.put(DatabaseHelper.CL_HO_TEN,hoVaTen);
-                nhanVienValue.put(DatabaseHelper.CL_AVATAR,avatar);
-                nhanVienValue.put(DatabaseHelper.CL_EMAIL,username);
-                id=cursor.getInt(0);
-                nhanVienValue.put(DatabaseHelper.CL_ID,id);
-                mDatabase.insert("HOSONHANVIEN",null,nhanVienValue);
-                Cursor cursorNhanVien=mDatabase.query("HOSONHANVIEN",null,DatabaseHelper.CL_ID+"=?",new String[]{String.valueOf(id)},null,null,null);
-                if(cursorNhanVien.moveToFirst())
-                {
-                    RegisterUser registerUser=new RegisterUser(cursorNhanVien.getString(2),cursorNhanVien.getInt(0));
+            Cursor cursor = mDatabase.query(DatabaseHelper.TB_TAI_KHOAN_NHAN_VIEN, null, DatabaseHelper.CL_USERNAME + "=?", new String[]{username}, null, null, null);
+            if (cursor.moveToFirst()) {
+                ContentValues nhanVienValue = new ContentValues();
+                nhanVienValue.put(DatabaseHelper.CL_HO_TEN, hoVaTen);
+                nhanVienValue.put(DatabaseHelper.CL_AVATAR, avatar);
+                nhanVienValue.put(DatabaseHelper.CL_EMAIL, username);
+                id = cursor.getInt(0);
+                nhanVienValue.put(DatabaseHelper.CL_ID, id);
+                mDatabase.insert("HOSONHANVIEN", null, nhanVienValue);
+                Cursor cursorNhanVien = mDatabase.query("HOSONHANVIEN", null, DatabaseHelper.CL_ID + "=?", new String[]{String.valueOf(id)}, null, null, null);
+                if (cursorNhanVien.moveToFirst()) {
+                    RegisterUser registerUser = new RegisterUser(cursorNhanVien.getString(2), cursorNhanVien.getInt(0));
                     return new Result.Success(registerUser);
                 }
                 cursorNhanVien.close();
@@ -52,7 +51,7 @@ public class RegisterDataSource {
             cursor.close();
             mDatabase.close();
 
-        return new Result.Error(new RuntimeException("Register failed"));
+            return new Result.Error(new RuntimeException("Register failed"));
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
         }
